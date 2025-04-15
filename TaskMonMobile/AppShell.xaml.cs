45f21de91303ec7eb00eb77b089f9fbad6c41896
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-04-15 16:29:16 +0300

    Initial commit

---
+++ TaskMonMobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskMonMobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TaskMonMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TaskMonMobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
