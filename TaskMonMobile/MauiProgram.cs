3b114478e9817e08d798617500fa114397f5880e
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-04-15 16:36:42 +0300

    Add UraniumUI

--- TaskMonMobile/MauiProgram.cs
+++ TaskMonMobile/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using UraniumUI;
 
 namespace TaskMonMobile;
 
@@ -9,6 +10,8 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseUraniumUI()
+            .UseUraniumUIMaterial() 
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
